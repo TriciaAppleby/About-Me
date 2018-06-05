77ceb508b2553a7018a64c0ab5016d79616c5d89
Tricia <triciaappleby06@gmail.com>
2018-06-05 18:54:29 -0500

    fininshed questions

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var name = prompt ('What is your name?');
 console.log('name'+ name);
 alert ('Hello '+ name +'! Please answer yes or no,');
 
-var question = prompt('ask a question here!');
-var sanitizedAnswer = question.toUpperCase().trim();
+var color = prompt('Is my favorite color pink?');
+var sanitizedAnswer = color.toUpperCase().trim();
 
 if (sanitizedAnswer ==='Y' || sanitizedAnswer ==='YES') {
     alert('No way!!');
@@ -14,12 +14,42 @@ else if (sanitizedAnswer === 'N' || sanitizedAnswer === 'NO') {
     alert('Good job!');
 }
 
-var nextquestion = prompt('ask question here!');
-var sanitizedAnswer = nextquestion.toUpperCase().trim();
+var age = prompt('Am I older than 40?');
+var sanitizedAnswer = age.toUpperCase().trim();
 
 if (sanitizedAnswer === 'Y'|| sanitizedAnswer === 'YES'){
-    alert ('yes!')
+    alert ('Really?!?')
 }
 else if (sanitizedAnswer === 'N' || sanitizedAnswer === 'NO'){
-    alert ('no!')
+    alert ('Nope, only 35!')
+}
+
+var pets = prompt('Do I like cats?');
+var sanitizedAnswer = pets.toUpperCase().trim();
+
+if (sanitizedAnswer === 'Y'|| sanitizedAnswer === 'YES'){
+    alert ('Yes!')
+}
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === 'NO'){
+    alert ('Sure do!')
+}
+
+var team = prompt('Do I like the Hawkeyes?');
+var sanitizedAnswer = team.toUpperCase().trim();
+
+if (sanitizedAnswer === 'Y'|| sanitizedAnswer === 'YES'){
+    alert ('Of course')
+}
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === 'NO'){
+    alert ('Who doesnt?')
+}
+
+var usa = prompt('Have I ever been outside of the country?');
+var sanitizedAnswer = usa.toUpperCase().trim();
+
+if (sanitizedAnswer === 'Y'|| sanitizedAnswer === 'YES'){
+    alert ('I wish!')
+}
+else if (sanitizedAnswer === 'N' || sanitizedAnswer === 'NO'){
+    alert ('Nope')
 }
